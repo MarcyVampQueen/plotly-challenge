26ab4dd4b57dcc4d9ea3d10a057664171d338990
MarcyVampQueen <savannahcordry@gmail.com>
2021-01-20 22:53:20 -0800

    bubble chart working

--- plots.js
+++ plots.js
@@ -1,12 +1,15 @@
-
-function unpack(rows, index) {
-    return rows.map(function (row) {
-        return row[index];
+function init() {
+    d3.json("samples.json").then(function (theData) {
+        for (let sub of theData.names) {
+            d3.select("#subjectInput").append("option").text(sub);
+        }
+        buildPage(theData.names[0]);
+    }
+    , function (error) {
+        console.log(error);
     });
 }
 
-
-// Add event listener for submit button
 d3.selectAll("#subjectInput").on("change", handleSubmit);
 
 // Submit Button handler
@@ -18,66 +21,71 @@ function handleSubmit() {
     var subject = d3.select("#subjectInput").node().value;
 
     // Update the Dashboard!
-    buildPlot(subject);
+    buildPage(subject);
 }
 
-// function updateDash(stock) {
-//     var apiKey = "YOUR API KEY HERE";
-//     var url = `https://www.quandl.com/api/v3/datasets/WIKI/${stock}.json?start_date=2016-10-01&end_date=2017-10-01&api_key=${apiKey}`;
-
-//     // update the components on the dashboard
-//     buildPlot(url);
-//     buildTable(url);
-// }
-
-function buildPlot(subject) {
+function buildPage(subject) {
     d3.json("samples.json").then(function (theData) {
         // Sort the data by values and then filter by subject
         theData.samples.sort((first, second) => first.sample_values - second.sample_values);
         filtered = theData.samples.filter(id => id.id === subject)[0];
-        console.log(filtered);
-        
+
         // Grab values from the response json object to build the plots
-        var values = filtered.sample_values.slice(0,10);
-        var ids = filtered.otu_ids.map(id => "OTU " + id).slice(0,10);
-        var labels = filtered.otu_labels.slice(0,10);
+        var values = filtered.sample_values;
+        var ids = filtered.otu_ids;
+        var labels = filtered.otu_labels;
 
-        var trace1 = {
-            type: "bar",
-            x: values,
-            y: ids,
-            orientation: 'h',
-            text: labels
-        };
-
-        var data = [trace1];
-
-        var layout = {
-            title: `Subject ${subject} OTUs`,
-            xaxis: {title:"Values"}
-        };
-
-        Plotly.newPlot("plot", data, layout);
-
-    });
-}
-
-function buildTable(url) {
-    // clear out the elements of the table first
-    var table = d3.select("#summary-table");
-    var tbody = table.select("tbody");
-    tbody.html("");
-
-    // call the API then fill in the elements of the table
-    d3.json(url).then(function (data) {
-        data.dataset.data.forEach(function (d) {
-            trow = tbody.append("tr");
-            trow.append("td").text(d[0]);
-            trow.append("td").text(d[1]);
-            trow.append("td").text(d[2]);
-            trow.append("td").text(d[3]);
-            trow.append("td").text(d[4]);
-            trow.append("td").text(d[5]);
+        // Build the charts and information
+        buildBarChart(values,ids,labels);
+        buildBubbleChart(values,ids,labels);
+        buildDemoInfo(theData, subject);
+    }
+        , function (error) {
+            console.log(error);
         });
-    })
 }
+
+function buildBarChart(values,ids,labels) {
+    var barChart = [{
+        type: "bar",
+        x: values.slice(0, 10),
+        y: ids.map(id => "OTU " + id).slice(0, 10),
+        orientation: 'h',
+        text: labels.slice(0, 10)
+    }];
+    var layout = {
+        title: `Subject OTUs`,
+        xaxis: { title: "Sample Count" }
+    };
+    Plotly.newPlot("bar", barChart, layout);
+}
+
+function buildBubbleChart(values,ids,labels) {
+    var bubbleChart = [{
+        x: ids,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
+            color:ids,
+            size: values
+        }
+    }];
+    layout = {
+        title: `Subject OTUs`,
+        xaxis: { title: "OTU ID" },
+        yaxis: { title: "Sample Count" }
+    };
+    Plotly.newPlot("bubble", bubbleChart, layout);
+}
+
+function buildDemoInfo(theData, subject) {
+    d3.select(".card-body").html('');
+    var demo = theData.metadata.filter(id => id.id === +subject)[0];
+    for (let x in demo){
+        var info = x + ": " + demo[x];
+        d3.select(".card-body").append("p").text(info);
+    }
+}
+
+init();
